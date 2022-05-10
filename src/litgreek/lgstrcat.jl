a9d416fa95e5a635c46e278c9408d0d597b4ed6c
Neel Smith <dnsmith.neel@gmail.com>
2022-05-10 08:40:32 -0400

    Past first 32 tests on strcat for literary Greek

--- src/litgreek/lgstrcat.jl
+++ src/litgreek/lgstrcat.jl
@@ -6,19 +6,123 @@
 $(SIGNATURES)
 """
 function strcat(s1::AbstractString,s2::AbstractString,ortho::LiteraryGreekOrthography)
-   if occursin(r"[πβφ]$", s1)
-    lg_appendtolabial(s1,s2)
+    if occursin(r"[πβφ]$", s1)
+        lg_appendtolabial(s1,s2)
 
-   else
-    s1 * s2
-   end
+    elseif occursin(r"[τδθ]$", s1)
+        lg_appendtodental(s1,s2)
+
+    elseif occursin(r"[κγχ]$", s1)
+        lg_appendtopalatal(s1,s2)
+
+    elseif endswith(s1, "ν")
+        lg_appendtonu(s1,s2)
+
+    else
+        s1 * s2
+    end
 end
 
 
-function lg_appendtolabial(s1, s2)
-    if ! occursin(r"^[μσ]", s2)
+"""Append string `s2` to a string ending in ν, `s1`.
+$(SIGNATURES)
+"""
+function lg_appendtonu(s1::AbstractString, s2::AbstractString)
+    if ! occursin(r"^[πβφψκγχξμλρσ]", s2)
         s1 * s2
         
+    elseif occursin(r"^[πβφ]", s2)
+        indices = collect(eachindex(s1))
+        quit = indices[end - 1]
+        string(s1[1:quit],"μ", s2)
+
+    elseif occursin(r"^[κγχξ]", s2)
+        indices = collect(eachindex(s1))
+        quit = indices[end - 1]
+        string(s1[1:quit],"γ", s2)
+
+    elseif startswith(s2, "μ")
+        indices = collect(eachindex(s1))
+        quit = indices[end - 1]
+        string(s1[1:quit],"μ", s2)
+
+    elseif startswith(s2, "λ")
+        indices = collect(eachindex(s1))
+        quit = indices[end - 1]
+        string(s1[1:quit],"λ", s2)
+
+    elseif startswith(s2, "ρ")
+        indices = collect(eachindex(s1))
+        quit = indices[end - 1]
+        string(s1[1:quit],"ρ", s2)
+
+    end
+
+end
+
+"""Append string `s2` to a string ending in a palatal, `s1`.
+$(SIGNATURES)
+"""
+function lg_appendtopalatal(s1::AbstractString, s2::AbstractString)
+    if ! occursin(r"^[μστδθ]", s2)
+        s1 * s2
+        
+    elseif startswith(s2, "μ")
+        indices = collect(eachindex(s1))
+        quit = indices[end - 1]
+        string(s1[1:quit],"γ", s2)
+
+    elseif startswith(s2, "σ")
+        indices1 = collect(eachindex(s1))
+        quit1 = indices1[end - 1]
+        indices2 = collect(eachindex(s2))
+        start2 = indices2[2]
+        string(s1[1:quit1],"ξ", s2[start2:end])        
+
+    elseif startswith(s2, "τ")
+        indices = collect(eachindex(s1))
+        quit = indices[end - 1]
+        string(s1[1:quit],"κ", s2)
+
+    elseif startswith(s2, "δ")
+        quit = indices[end - 1]
+        string(s1[1:quit],"γ", s2)
+
+    elseif startswith(s2, "θ")
+        indices = collect(eachindex(s1))
+        quit = indices[end - 1]
+        string(s1[1:quit],"χ", s2)
+
+
+    end
+end
+
+
+
+"""Append string `s2` to a string ending in a dental, `s1`.
+$(SIGNATURES)
+"""
+function lg_appendtodental(s1::AbstractString, s2::AbstractString)
+    if ! occursin(r"^[τδθ]", s2)
+        s1 * s2
+
+
+
+    else
+        indices = collect(eachindex(s1))
+        quit = indices[end - 1]
+        string(s1[1:quit],"σ", s2)
+    end
+end
+
+
+"""Append string `s2` to a string ending in a labial, `s1`.
+$(SIGNATURES)
+"""
+function lg_appendtolabial(s1::AbstractString, s2::AbstractString)
+    if ! occursin(r"^[μστδθ]", s2)
+        s1 * s2
+
     elseif startswith(s2, "μ")
         indices = collect(eachindex(s1))
         quit = indices[end - 1]
@@ -30,5 +134,21 @@ function lg_appendtolabial(s1, s2)
         indices2 = collect(eachindex(s2))
         start2 = indices2[2]
         string(s1[1:quit1],"ψ", s2[start2:end])        
+
+    elseif startswith(s2, "τ")
+        indices = collect(eachindex(s1))
+        quit = indices[end - 1]
+        string(s1[1:quit],"π", s2)
+
+    elseif startswith(s2, "δ")
+        quit = indices[end - 1]
+        string(s1[1:quit],"β", s2)
+
+    elseif startswith(s2, "θ")
+        indices = collect(eachindex(s1))
+        quit = indices[end - 1]
+        string(s1[1:quit],"φ", s2)
+
+
     end
 end
--- test/test_strcat.jl
+++ test/test_strcat.jl
@@ -1,38 +1,52 @@
 @testset "Test concatenating strings for literary Greek" begin
     ortho = literaryGreek()
 
-    @test_broken strcat("λελειπ", "μαι", ortho) == "λελειμμαι"
-    @test_broken strcat("λελειπ", "σαι", ortho) == "λελειψαι"
-    @test_broken strcat("λελειπ", "ται", ortho) == "λελειπται"
+    @test strcat("λελειπ", "μαι", ortho) == "λελειμμαι"
+    @test strcat("λελειπ", "σαι", ortho) == "λελειψαι"
+    @test strcat("λελειπ", "ται", ortho) == "λελειπται"
 
-    @test_broken strcat("τετριβ", "μαι", ortho) == "τετριμμαι"
-    @test_broken strcat("τετριβ", "σαι", ortho) == "τετριψαι"
-    @test_broken strcat("τετριβ", "ται", ortho) == "τετριπται"
-
-
-    @test_broken strcat("γεγραφ", "μαι", ortho) == "γραμμαι"
-    @test_broken strcat("γεγραφ", "σαι", ortho) == "γραψαι"
-    @test_broken strcat("γεγραφ", "ται", ortho) == "γραπται"
+    @test strcat("τετριβ", "μαι", ortho) == "τετριμμαι"
+    @test strcat("τετριβ", "σαι", ortho) == "τετριψαι"
+    @test strcat("τετριβ", "ται", ortho) == "τετριπται"
 
 
-    @test_broken strcat("πεπλεκ", "μαι", ortho) == "πεπλεγμαι"
-    @test_broken strcat("πεπλεκ", "σαι", ortho) == "πεπλεξαι"
-    @test_broken strcat("πεπλεκ", "ται", ortho) == "πεπλεκται"
+    @test strcat("γεγραφ", "μαι", ortho) == "γεγραμμαι"
+    @test strcat("γεγραφ", "σαι", ortho) == "γεγραψαι"
+    @test strcat("γεγραφ", "ται", ortho) == "γεγραπται"
+
+
+    @test strcat("πεπλεκ", "μαι", ortho) == "πεπλεγμαι"
+    @test strcat("πεπλεκ", "σαι", ortho) == "πεπλεξαι"
+    @test strcat("πεπλεκ", "ται", ortho) == "πεπλεκται"
    
-    @test_broken strcat("λελεγ", "μαι", ortho) == "λελεγμαι"
-    @test_broken strcat("λελεγ", "σαι", ortho) == "λελεξαι"
-    @test_broken strcat("λελεγ", "ται", ortho) == "λελεκται"
+    @test strcat("λελεγ", "μαι", ortho) == "λελεγμαι"
+    @test strcat("λελεγ", "σαι", ortho) == "λελεξαι"
+    @test strcat("λελεγ", "ται", ortho) == "λελεκται"
 
-    @test_broken strcat("βεβρεχ", "μαι", ortho) == "βεβρεγμαι"
-    @test_broken strcat("βεβρεχ", "σαι", ortho) == "βεβρεξαι"
-    @test_broken strcat("βεβρεχ", "ται", ortho) == "βεβρεκται"
-
-
-   
+    @test strcat("βεβρεχ", "μαι", ortho) == "βεβρεγμαι"
+    @test strcat("βεβρεχ", "σαι", ortho) == "βεβρεξαι"
+    @test strcat("βεβρεχ", "ται", ortho) == "βεβρεκται"
 
 
-    @test_broken strcat("λειπ", "σω", ortho) == "λειψω"
-    @test_broken strcat("τριβ", "σω", ortho) == "τριψω"
-    @test_broken strcat("γραφ", "σω", ortho) == "γραψω"
-    
+    @test strcat("πεπειθ", "ται", ortho) == "πεπεισται"
+
+    @test strcat("λειπ", "σω", ortho) == "λειψω"
+    @test strcat("τριβ", "σω", ortho) == "τριψω"
+    @test strcat("γραφ", "σω", ortho) == "γραψω"
+
+
+    @test strcat("ἐν","πιπτω", ortho) == "ἐμπιπτω"
+    @test strcat("ἐν","βαλλω", ortho) == "ἐμβαλλω"
+    @test strcat("ἐν","φαινω", ortho) == "ἐμφαινω"
+
+    @test strcat("ἐν","καλεω", ortho) == "ἐγκαλεω"
+    @test strcat("ἐν","γραφω", ortho) == "ἐγγραφω"
+    @test strcat("συν","χεω", ortho) == "συγχεω"
+    @test strcat("συν","ξυω", ortho) == "συγξυω"
+
+    @test strcat("ἐν","μενω", ortho) == "ἐμμενω"
+
+    @test strcat("συν","λογος", ortho) == "συλλογος"
+    @test strcat("συν","ρεω", ortho) == "συρρεω"
+
 end
