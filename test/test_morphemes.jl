8508337313534905202d2b68ff4b2fee945ef017
Neel Smith <dnsmith.neel@gmail.com>
2022-05-14 08:58:21 -0400

    set up redupe tests

--- test/test_morphemes.jl
+++ test/test_morphemes.jl
@@ -1,4 +1,4 @@
-@testset "Test honoring of morpheme boundaries" begin
+@testset "Test honoring morpheme boundaries in augment" begin
     ortho = literaryGreek()
 
     a1 = augment("ἐν#κελευε", ortho)
@@ -9,4 +9,18 @@
 
     a3 = augment("κελευε", ortho)
     @test a3 == nfkc("ἐκελευε")
+end
+
+
+@testset "Test honoring morpheme boundaries in reduplication" begin
+    ortho = literaryGreek()
+
+    a1 = reduplicate("ἐν#κελευκε", ortho)
+    @test a1 == nfkc("ἐγκεκελευκε")
+
+    a2 = augment("ἐν#αγγελκε", ortho)
+    @test a2 == nfkc("ἐνηγγελκε")
+
+    a3 = augment("κελευκε", ortho)
+    @test a3 == nfkc("ἐκεκελευκε")
 end
