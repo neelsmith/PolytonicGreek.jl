4a8deb437905f3b95ce28ef94e6d31afcd65d67b
Neel Smith <dnsmith.neel@gmail.com>
2023-07-23 11:59:10 -0400

    Spec morph simulatoin tests

---
+++ test/test_morphsim.jl
@@ -0,0 +1,28 @@
+@testset "Simulate phonology used in morphological generation" begin
+    o = literaryGreek()
+
+    ex1 = strcat("λυ", "ει", o)
+    @test ex1 ==  "λυει"
+    @test accentword(ex1,:RECESSIVE, o) == "λύει"
+
+    dupe = reduplicate("λυ", o)
+    @test dupe ==  "λελυ"
+    @test accentword(strcat(dupe, "κε", o), :RECESSIVE, o) ==  "λέλυκε"
+
+    aug = augment("λυ", o)
+    @test aug == "ἐλυ"
+    @test accentword(strcat(aug, "ομεθα",o), :RECESSIVE, o) ==  "ἐλυόμεθα"
+
+    ex2 = strcat("ἀνα#λυ", "ει", o)
+    @test ex2 == "ἀνα#λυει"
+    @test accentword(ex2,:RECESSIVE, o) == "ἀναλύει"
+
+    dupe2 = reduplicate("ἀνα#λυ", o)
+    @test_broken dupe2 ==  "ἀνα#λελυ"
+    @test accentword(strcat(dupe2, "κε", o), :RECESSIVE, o) ==  "ἀναλέλυκε"
+
+    aug2 = augment("ἀνα#λυ", o)
+    @test_broken aug == "ἀνα#ἐλυ"
+    @test accentword(strcat(aug2, "ομεθα",o), :RECESSIVE, o) ==  "ἀνελυόμεθα"
+
+end
