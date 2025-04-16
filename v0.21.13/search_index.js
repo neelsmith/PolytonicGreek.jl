d88506dbd41f06957b7e3f412c04874d509640f8
Documenter.jl <documenter@juliadocs.github.io>
2025-04-16 14:41:13 +0000

    build based on 9441993

---
+++ v0.21.13/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#PolytonicGreek.jl","page":"Home","title":"PolytonicGreek.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Rigorously defined orthographies for text in polytonic ancient Greek. ","category":"page"},{"location":"","page":"Home","title":"Home","text":"Project documentation is available on quartopub.","category":"page"}]
+}
--- versions.js
+++ versions.js
@@ -18,5 +18,5 @@ var DOC_VERSIONS = [
   "v0.6",
   "dev",
 ];
-var DOCUMENTER_NEWEST = "v0.21.10";
+var DOCUMENTER_NEWEST = "v0.21.13";
 var DOCUMENTER_STABLE = "stable";
